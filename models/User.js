e569c642f5f76590db07b9b0eaad955495070135
leoazcp1 <leo.azcp1@gmail.com>
2025-11-19 19:28:12 -0300

    Exercise Tracker FCC

--- models/User.js
+++ models/User.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 
 const exerciseSchema = new mongoose.Schema({
   description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: Date, required: true },
+  duration:   { type: Number, required: true },
+  date:       { type: Date,   required: true }
 });
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  log: [exerciseSchema],
+  log:      [exerciseSchema]   // <-- array de ejercicios
 });
 
 module.exports = mongoose.model('User', userSchema);
